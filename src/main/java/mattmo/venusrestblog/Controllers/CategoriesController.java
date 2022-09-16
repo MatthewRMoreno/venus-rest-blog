97759bdfcc425416d418c362a48c5e9a9e313c74
Matthew Moreno <matthew.r.moreno21@gmail.com>
2022-09-16 15:30:16 -0500

    Complete: Finished categories controller

--- src/main/java/mattmo/venusrestblog/Controllers/CategoriesController.java
+++ src/main/java/mattmo/venusrestblog/Controllers/CategoriesController.java
@@ -1,29 +1,37 @@
 package mattmo.venusrestblog.Controllers;
 
+import lombok.AllArgsConstructor;
 import mattmo.venusrestblog.data.Category;
 import mattmo.venusrestblog.data.Post;
+import mattmo.venusrestblog.repository.CategoriesRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping(value = "/api/categories", produces = "application/json")
 public class CategoriesController {
+    private CategoriesRepository categoriesRepository;
 
     @GetMapping("")
-    private Category fetchPostsByCategory(@RequestParam String categoryName) {
-        Category category = new Category(1L, categoryName, null);
-
-        ArrayList<Post> fakePosts = new ArrayList<>();
-        fakePosts.add(new Post(1L, "Bunnies Rock!", "for real!", null, new ArrayList<>(Arrays.asList(new Category(1L, categoryName, null)))));
-        fakePosts.add(new Post(2L, "Get a life", "for really real", null, new ArrayList<>(Arrays.asList(new Category(1L, categoryName, null)))));
-        category.setPosts(fakePosts);
-
-        return category;
+    private List<Category> fetchAllCategories() {
+        return categoriesRepository.findAll();
     }
 
+    @GetMapping("/search")
+    private Category fetchCategoryByCategoryName(@RequestParam String categoryName) {
+        Category cat = categoriesRepository.findByName(categoryName);
+        if(cat == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category " + categoryName + " not found");
+        }
+        return cat;
+    }
 }

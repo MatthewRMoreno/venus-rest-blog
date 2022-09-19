9c9ace6277a1877410e25eebdb6dd3285c784e59
Matthew Moreno <matthew.r.moreno21@gmail.com>
2022-09-19 15:26:18 -0500

    Complete: Added new classes

--- src/main/java/mattmo/venusrestblog/security/UserService.java
+++ src/main/java/mattmo/venusrestblog/security/UserService.java
@@ -1,5 +1,6 @@
 package mattmo.venusrestblog.security;
 
+import mattmo.venusrestblog.data.User;
 import mattmo.venusrestblog.repository.UsersRepository;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
